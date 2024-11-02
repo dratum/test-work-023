01c1b5595ee104a8eb497c03b231d8e793c83a78
dratumz <dratumzh187@gmail.com>
2024-11-02 22:15:22 +0300

    finally add api
    edit
    
    edit
    
    edit
    
    finally? edit env
    
    edit finally
    
    without env
    
    change

--- api/send.js
+++ api/send.js
@@ -1,9 +1,4 @@
 export default function handler(req, res) {
-  if (req.method === "POST") {
-    const data = req.body;
-    res.status(200).json({ name: data.name });
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  const data = req.body;
+  res.status(200).json({ name: data.name });
 }
