f7e7b84790f9759e4ba0db2f9d97f47480bcebb4
dratumz <dratumzh187@gmail.com>
2024-11-03 16:34:20 +0300

    edit api route

--- api/send.js
+++ api/send.js
@@ -1,6 +1,15 @@
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
   if (req.method === "POST") {
     const data = req.body;
+    console.log("Received data:", data);
     res.status(200).json({ name: data.name });
   } else {
     res.setHeader("Allow", ["POST"]);
