0be04433a63c9562f0cdd8f16cbdbafdf075376a
dratumz <dratumzh187@gmail.com>
2024-11-03 16:55:48 +0300

    edit api

--- api/send.js
+++ api/send.js
@@ -1,18 +1,8 @@
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  console.log(req.method);
 
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  if (req.method === "POST") {
-    const data = req.body;
-    console.log("Received data:", data);
-    res.status(200).json({ name: data.name });
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
+  const data = req.body;
+  console.log(data);
+
+  res.status(200).json({ name: data.name });
 }
